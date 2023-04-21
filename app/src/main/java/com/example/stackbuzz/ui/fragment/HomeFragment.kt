7dc10db9fa6ab2afd0b481bfd372bd87cc9d11d9
hkbettiah@gmail.com <hkbettiah@gmail.com>
2023-04-21 10:32:10 +0530

    dynamic tags

--- app/src/main/java/com/example/stackbuzz/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/stackbuzz/ui/fragment/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.AsyncListDiffer
@@ -17,6 +18,9 @@ import com.example.stackbuzz.data.model.QuestionItem
 import com.example.stackbuzz.R
 import com.example.stackbuzz.data.api.ApiRepository
 import com.example.stackbuzz.databinding.FragmentHomeBinding
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipDrawable
+import com.google.android.material.chip.ChipGroup
 
 
 class HomeFragment : Fragment() {
@@ -34,7 +38,7 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
 
@@ -98,18 +102,50 @@ class HomeFragment : Fragment() {
                 .load(question.owner.profile_image)
                 .centerCrop()
                 .placeholder(R.drawable.profile_pic)
-                .into(holder.image);
+                .into(holder.image)
+
+            val chipGroup = ChipGroup(context).apply {
+                id = View.generateViewId()
+                chipSpacingVertical = 8
+                chipSpacingHorizontal = 8
+            }
+
+            for (tag in question.tags!!) {
+                val chip = Chip(context).apply {
+                    text = tag
+                    setChipDrawable(
+                        ChipDrawable.createFromAttributes(
+                            context,
+                            null,
+                            0,
+                            com.google.android.material.R.style.Widget_Material3_Chip_Suggestion
+                        )
+                    )
+
+                    textStartPadding = 0F
+                    textEndPadding = 0F
+                    textSize = 10F
+                    id = View.generateViewId()
+                    setChipStrokeColorResource(R.color.transparent)
+                }
+                chipGroup.addView(chip)
+            }
+
+            holder.tagHolder.removeAllViews()
+            holder.tagHolder.addView(chipGroup)
         }
 
         override fun getItemCount() = mainDiffer.currentList.size
 
-        inner class ViewHolder internal constructor(view: View) : RecyclerView.ViewHolder(view) {
+        inner class ViewHolder internal constructor(view: View) :
+            RecyclerView.ViewHolder(view) {
             internal val title: TextView = view.findViewById(R.id.title_question)
             internal val image: ImageView = view.findViewById(R.id.imageview_question)
             internal val username: TextView = view.findViewById(R.id.username_question)
             internal val score: TextView = view.findViewById(R.id.score_question)
             internal val answerCount: TextView = view.findViewById(R.id.answer_count_question)
             internal val viewCount: TextView = view.findViewById(R.id.view_count_question)
+            internal val tagHolder: LinearLayout = view.findViewById(R.id.chip_tag_holder)
         }
     }
 }
